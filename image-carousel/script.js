73480436c1e34340e768147ec26bd01d1c42672a
lukes8 <gw.lusi@seznam.cz>
2022-09-22 18:28:26 +0200

    Added first project - image-carousel

---
+++ image-carousel/script.js
@@ -0,0 +1,43 @@
+const imgs = document.getElementById('imgs')
+const left = document.getElementById('left')
+const right = document.getElementById('right')
+
+const imgList = document.querySelectorAll('#imgs img')
+
+let idx = 0
+
+let interval = setInterval(run, 2000)
+
+function run () {
+    
+    changeImage()
+    idx++
+}
+
+function changeImage() {
+    if (idx > imgList.length - 1) {
+        idx = 0
+    } else if (idx < 0) {
+        idx = imgList.length - 1
+    }
+
+    imgs.style.transform = `translateX(${-idx * 500}px)`
+}
+
+left.addEventListener('click', () => {
+    idx--
+    changeImage()
+    resetInterval()
+})
+
+right.addEventListener('click', ()=> {
+    idx++
+    changeImage()
+    resetInterval()
+})
+
+function resetInterval() {
+    clearInterval(interval)
+    interval = setInterval(run, 2000)
+
+}
